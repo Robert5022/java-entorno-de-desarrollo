d64c792317ba6839474a25f6105ddd7923d32a78
unknown <Robert5022>
2024-03-14 16:14:28 +0100

    Subir personas

---
+++ SubirPersonas/src/subida/Persona.java
@@ -0,0 +1,109 @@
+package subida;
+import java.util.*;
+
+/**
+ * 
+ */
+public class Persona {
+
+    /**
+     * Default constructor
+     */
+    public Persona() {
+    }
+
+    /**
+     * 
+     */
+    private String dni;
+
+    /**
+     * 
+     */
+    private String nombre;
+
+    /**
+     * 
+     */
+    private String apellidos;
+
+    /**
+     * 
+     */
+    private int edad;
+
+    /**
+     * @param dni 
+     * @param nombre 
+     * @param apellidos 
+     * @param edad
+     */
+    public Persona(String dni, String nombre, String apellidos, int edad) {
+        // TODO implement here
+    }
+
+    /**
+     * @param dni Modifica el DNI
+     */
+    public void setDni(String dni) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public String getDni() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @param Nombre
+     */
+    public void SetNombre(String Nombre) {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Operation3() {
+        // TODO implement here
+    }
+
+    /**
+     * @param Nombre
+     */
+    public void GetNombre(String Nombre) {
+        // TODO implement here
+    }
+
+    /**
+     * @param Apellido
+     */
+    public void SetApellido(String Apellido) {
+        // TODO implement here
+    }
+
+    /**
+     * @param Apellido
+     */
+    public void GetApellido(String Apellido) {
+        // TODO implement here
+    }
+
+    /**
+     * @param Edad
+     */
+    public void SetEdad(int Edad) {
+        // TODO implement here
+    }
+
+    /**
+     * @param Edad
+     */
+    public void GetEdad(int Edad) {
+        // TODO implement here
+    }
+
+}
