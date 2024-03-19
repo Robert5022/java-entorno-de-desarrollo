cba6fa67b461b5552cce78b9cb8d29f1c179335e
unknown <Robert5022>
2024-03-19 17:31:48 +0100

    Main con todo importado

---
+++ Paquete/src/Paquete/Paquete.java
@@ -0,0 +1,43 @@
+package Paquete;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Paquete {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Paquete window = new Paquete();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Paquete() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
